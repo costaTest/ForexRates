ac67fafafc16f84bfd156c1b3178cf447b1aeec9
Costa <costa365@gmail.com>
2022-03-20 12:06:44 +0200

    Fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText("Forex Rates");
+  expect(element).toBeInTheDocument();
+});
+
+test('renders USD EUR currency', () => {
+  render(<App />);
+  const element = screen.getByText("USD / EUR");
+  expect(element).toBeInTheDocument();
 });
